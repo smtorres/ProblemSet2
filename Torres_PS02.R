4880e7e2d245969deeaf515a589f5a3717ff6f44
Michelle Torres <smtorres@wustl.edu>
2014-02-05 01:57:10 -0600

    Task 2
    Task 2 Plus Changes…

--- Torres_PS02.R
+++ Torres_PS02.R
@@ -8,7 +8,7 @@
 #       cho.gains = TRUE to obtain Cho-Gains statistic
 #       DEFAULT: No statistic
 
-bendford <- function(x, leemis.m=FALSE, cho.gains=FALSE){ 
+benford <- function(x, leemis.m=FALSE, cho.gains=FALSE){ 
   x<-as.vector(x)
   vec<-1:length(x)
   for (i in 1:length(x)){
@@ -25,25 +25,50 @@ bendford <- function(x, leemis.m=FALSE, cho.gains=FALSE){
   leemis<-max(leemvec)*sqrt(length(vec))
   chogains<- sqrt(sum(chovec))*sqrt(length(vec))
   if(leemis.m==TRUE & cho.gains==TRUE){
-    list<-list(leemis, chogains, cbind(nums, totals))
+    listr<-list(leemis, chogains, cbind(nums, totals))
     print(paste("Leemis=", leemis))
     print(paste("Cho-Gains=", chogains))
-    return(list)
+    return(listr)
   }
   else if(leemis.m==FALSE & cho.gains==TRUE){
-    list<- list(chogains, cbind(nums, totals))
+    listr<- list(chogains, cbind(nums, totals))
     print(paste("Cho-Gains=", chogains))
-    return(list)
+    return(listr)
   }
   else if(leemis.m==TRUE & cho.gains==FALSE){
-    list<- list(leemis, cbind(nums, totals))
+    listr<- list(leemis, cbind(nums, totals))
     print(paste("Leemis=", leemis))
-    return(list)
+    return(listr)
   }
   else if (leemis.m==FALSE & cho.gains==FALSE){
-    list<-list(cbind(nums,totals))
+    listr<-list(cbind(nums,totals))
     print("No statistics requested")
-    return(list)    
+    return(listr)    
   } else{}
 }
 
+## PROBLEM 2
+##FUNCTION "print.benford"
+## Returns a table with the values of Leemis' m and Cho-Gains' d statistics
+## and determines if they are significant in order to reject the null
+##hypothesis "no fraud" from the Benford Distribution 
+## Input: x= vector or matrix of electoral results
+## Output: table with the values for each m and d, significance at 
+# 0.01, 0.5 and 0.1
+print.benfords<-function(x) { 
+  stats<-benford(x, leemis.m=TRUE, cho.gains=TRUE)
+  Statistic<- c("Leemis'm", "Cho-Gains' d")
+  Legend<- c("CI: *0.10, **0.05, ***0.01", "","")
+  Value<-c(stats[[1]], stats[[2]])
+  mystars.l <- ifelse(stats[[1]] >= 1.212, "***", 
+                ifelse((stats[[1]]<1.212 && stats[[1]]>=967), "** ", 
+                  ifelse((stats[[1]]>=0.851 && stats[[1]] < 967), "* ", " ")))
+  mystars.c <- ifelse(stats[[2]] >= 1.569, "***", 
+                  ifelse((stats[[2]] >= 1.33), "** ", 
+                    ifelse(stats[[2]] >= 1.212, "* ", " ")))
+  Significance<-c(mystars.l, mystars.c)
+  results<-cbind(Statistic, Value, Significance)
+  results<-rbind(results,Legend)
+  return(results)
+  
+}
